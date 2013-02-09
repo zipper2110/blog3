f1106cbfbdb3df48b2756bcbe0c199c0e6475842
zipper2110 <leetvin@gmail.com>
2013-02-09 23:05:58 +0600

    commining

---
+++ application/logs/2013/02/09.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2013-02-09 20:42:07 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: webdav/db.php ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-02-09 20:42:07 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: webdav/db.php ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\blog3\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-02-09 22:42:55 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: webdav/db.php ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-02-09 22:42:55 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: webdav/db.php ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\blog3\index.php(109): Kohana_Request->execute()
+#1 {main}
